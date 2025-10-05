852206e2ab473d1da56a5d1cb8f1199cee33e877
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-05 16:57:19 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -47,7 +47,7 @@ module.exports.run = async function({ api, event }) {
 └───────────────⭓
  `,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
- }, event.threadID, () => fs.unlinkSync(__dirn"));
+ }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
  return request("https://i.imgur.com/5otYwvn.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
