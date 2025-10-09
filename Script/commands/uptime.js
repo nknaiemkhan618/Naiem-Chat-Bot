6c2da142b7603279fae58210f9dc41224fb91306
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-09 21:10:58 +0600

    Update uptime.js

--- Script/commands/uptime.js
+++ Script/commands/uptime.js
@@ -7,7 +7,7 @@ module.exports = {
     name: "uptime",
     version: "2.0.0",
     hasPermssion: 0,
-    credits: "SHAHADAT SAHU",
+    credits: "SHAHADAT U",
     description: "Show advanced system uptime.",
     commandCategory: "system",
     usages: "uptime",
