66737c45ccd19d56cda4a2354fbff53c08ba9e23
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-10 01:41:29 +0600

    Update pending.js

--- Script/commands/pending.js
+++ Script/commands/pending.js
@@ -12,7 +12,7 @@ module.exports.languages = {
   "en": {
     "invaildNumber": "❌ %1 is not a valid number",
     "cancelSuccess": "✅ Successfully rejected %1 group(s)!",
-    "notiBox1": "চ্ঁলে্ঁ এ্ঁসে্ঁছি্ঁ ─꯭─⃝‌‌না্ঁঈ্ঁমে্ঁর্ঁ ফে্ঁমা্ঁস্ঁ ব্ঁট্ঁ এঁখঁনঁ তোঁমাঁদেঁরঁ সাঁথেঁ আঁড্ডাঁ দিঁবঁ..!😘",
+    "notiBox1": "চ্ঁলে্ঁ এ্ঁসে্ঁছি্ঁ ─꯭─⃝আ্ঁশ্ঁরা্ঁফু্ঁল্ঁ এ্ঁর্ঁ ফে্ঁমা্ঁস্ঁ ব্ঁট্ঁ এঁখঁনঁ তোঁমাঁদেঁরঁ সাঁথেঁ আঁড্ডাঁ দিঁবঁ..!😘",
     "notiBox2": `╭•┄┅═══❁🌺❁═══┅┄•╮
      আ্ঁস্ঁসা্ঁলা্ঁমু্ঁ💚আ্ঁলা্ঁই্ঁকু্ঁম্ঁ
 ╰•┄┅═══❁🌺❁═══┅┄•╯
@@ -26,7 +26,7 @@ ${global.config.PREFIX}info
 ${global.config.PREFIX}admin
 
 ★ For any help or complaints, please contact admin ★
-➤ 𝐌𝐞𝐬𝐬𝐞𝐧𝐠𝐞𝐫: https://m.me/nk.naiem.khan.641816
+➤ 𝐌𝐞𝐬𝐬𝐞𝐧𝐠𝐞𝐫: https://m.me/61581203436353?
 ➤ 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: https://wa.me/8801908143017
 
 ❖⋆═══════════════════════⋆❖
