22a72d17b5a36bd6004fad7f8df58b5d91c2a42a
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-09 07:06:14 +0600

    Update Wearefriends.js

--- Script/commands/Wearefriends.js
+++ Script/commands/Wearefriends.js
@@ -10,7 +10,7 @@ module.exports.config = {
 };
 
 // === এখানে আপনার লিংক সেট করুন ===
-const fbGroup = "👉 আমাদের Facebook Page: https://www.facebook.com/share/p/15swwzNxDm/";
+const fbGroup = "👉 আমাদের ok Page: https://www.facebook.com/share/p/15swwzNxDm/";
 const fbPage = "👉 আমাদের Facebook Group: https://www.facebook.com/profile.php?id=61580237348388";
 const tiktok = "👉 আমাদের TikTok ID: https://vt.tiktok.com/ZSDcGf8KH/";
 
