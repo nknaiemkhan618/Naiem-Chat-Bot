1bbcf4a0efb9c42bb0bb3a42b8c7791f9ab30d53
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-12 03:56:09 +0600

    Update spam.js

--- Script/commands/spam.js
+++ Script/commands/spam.js
@@ -11,7 +11,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
- const permission = ["61580237348388"];
+ const permission = ["61581203436353"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
  if (args.length !== 2) {
