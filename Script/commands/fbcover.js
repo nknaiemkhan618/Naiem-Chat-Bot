8d5dda0f2452d07dd5fb3eae4fa412429d1d6470
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-15 23:12:24 +0600

    Refactor fbcover.js for improved functionality

--- Script/commands/fbcover.js
+++ Script/commands/fbcover.js
@@ -1,59 +1,76 @@
 const axios = require("axios");
+
 const baseApiUrl = async () => {
- const _0x15493d = await axios.get("https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json");
- return _0x15493d.data.api;
+ const base = await axios.get(
+ `https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`,
+ );
+ return base.data.api;
 };
+
 module.exports.config = {
- 'name': "fbcover",
- 'version': "6.9",
- 'hasPermission': 0x0,
- 'credits': "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
- 'description': "Facebook cover",
- 'commandCategory': " cover",
- 'usages': "name - title - address - email - phone - color (default = white)",
- 'cooldowns': 0x5
+ name: "fbcover",
+ version: "6.9",
+ hasPermssion: 0,
+ credits: "Dipto",
+ description: "Facebook cover",
+ usePrefix: true,
+ prefix: true,
+ commandCategory: "Cover",
+ category: " cover",
+ usages: "name - title - address - email - phone - color (default = white)",
+ cooldowns: 5,
 };
-module.exports.run = async function ({
- api: _0x1cb5ab,
- event: _0x39784b,
- args: _0x99677d,
- Users: _0x27cfb6
-}) {
- const _0x486dd8 = _0x99677d.join(" ");
- let _0x47416f;
- if (_0x39784b.type === "message_reply") {
- _0x47416f = _0x39784b.messageReply.senderID;
+module.exports.run = async function ({ api, event, args, Users }) {
+ const dipto = args.join(" ");
+ let id;
+ if (event.type === "message_reply") {
+ id = event.messageReply.senderID;
  } else {
- _0x47416f = Object.keys(_0x39784b.mentions)[0] || _0x39784b.senderID;
+ id = Object.keys(event.mentions)[0] || event.senderID;
  }
- var _0x2d3b59 = await _0x27cfb6.getNameUser(_0x47416f);
- if (!_0x486dd8) {
- return _0x1cb5ab.sendMessage("you can see and try this system Create Your Facebook Cover " + global.config.PREFIX + "fbcover v1/v2/v3 - name - title - address - email - phone - color (default = white)", _0x39784b.threadID, _0x39784b.messageID);
+ var nam = await Users.getNameUser(id);
+ if (!dipto) {
+ return api.sendMessage(
+ `❌| wrong \ntry ${global.config.PREFIX}fbcover v1/v2/v3 - name - title - address - email - phone - color (default = white)`,
+ event.threadID,
+ event.messageID,
+ );
  } else {
- const _0x1bc6cb = _0x486dd8.split('-');
- const _0xb7cc01 = _0x1bc6cb[0].trim() || 'v1';
- const _0x3ea3dd = _0x1bc6cb[1].trim() || " ";
- const _0x6252a8 = _0x1bc6cb[2].trim() || " ";
- const _0x3744c5 = _0x1bc6cb[3].trim() || " ";
- const _0x1bb59a = _0x1bc6cb[4].trim() || " ";
- const _0x34c576 = _0x1bc6cb[5].trim() || " ";
- const _0x34e9f5 = _0x1bc6cb[6].trim() || "white";
- _0x1cb5ab.sendMessage("Processing your cover,Wait", _0x39784b.threadID, (_0x5e0253, _0x51abd0) => setTimeout(() => {
- _0x1cb5ab.unsendMessage(_0x51abd0.messageID);
- }, 4000));
- const _0x226845 = (await baseApiUrl()) + "/cover/" + _0xb7cc01 + "?name=" + encodeURIComponent(_0x3ea3dd) + "&subname=" + encodeURIComponent(_0x6252a8) + "&number=" + encodeURIComponent(_0x34c576) + "&address=" + encodeURIComponent(_0x3744c5) + "&email=" + encodeURIComponent(_0x1bb59a) + "&colour=" + encodeURIComponent(_0x34e9f5) + "&uid=" + _0x47416f;
+ const msg = dipto.split("-");
+ const v = msg[0].trim() || "v1";
+ const name = msg[1].trim() || " ";
+ const subname = msg[2].trim() || " ";
+ const address = msg[3].trim() || " ";
+ const email = msg[4].trim() || " ";
+ const phone = msg[5].trim() || " ";
+ const color = msg[6].trim() || "white";
+ api.sendMessage(
+ `Processing your cover,Wait koro baby < 😘`,
+ event.threadID,
+ (err, info) =>
+ setTimeout(() => {
+ api.unsendMessage(info.messageID);
+ }, 4000),
+ );
+ const img = `${await baseApiUrl()}/cover/${v}?name=${encodeURIComponent(name)}&subname=${encodeURIComponent(subname)}&number=${encodeURIComponent(phone)}&address=${encodeURIComponent(address)}&email=${encodeURIComponent(email)}&colour=${encodeURIComponent(color)}&uid=${id}`;
+
  try {
- const _0x1598a7 = await axios.get(_0x226845, {
- 'responseType': "stream"
- });
- const _0xf0c350 = _0x1598a7.data;
- _0x1cb5ab.sendMessage({
- 'body': "⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆\n✧⃝•🩷𝗙𝗜𝗥𝗦𝗧 𝗡𝗔𝗠𝗘: " + _0x3ea3dd + "\n✧⃝•💜𝗦𝗘𝗖𝗢𝗡𝗗 𝗡𝗔𝗠𝗘:" + _0x6252a8 + "\n✧⃝•🤍𝗔𝗗𝗗𝗥𝗘𝗦𝗦: " + _0x3744c5 + "\n✧⃝•💛𝗠𝗔𝗜𝗟: " + _0x1bb59a + "\n✧⃝•❤️‍🩹𝗣𝗛𝗢𝗡𝗘 𝗡𝗢.: " + _0x34c576 + "\n✧⃝•💖𝗖𝗢𝗟𝗢𝗥: " + _0x34e9f5 + "\n✧⃝•❤️𝗨𝗦𝗘𝗥 𝗡𝗔𝗠𝗘: " + _0x2d3b59 + "\n✧⃝•💛𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : " + _0xb7cc01 + "\n⋆✦⋆⎯⎯⎯⎯⎯⎯⎯⎯⎯⋆✦⋆",
- 'attachment': _0xf0c350
- }, _0x39784b.threadID, _0x39784b.messageID);
- } catch (_0x5d9b8d) {
- console.error(_0x5d9b8d);
- _0x1cb5ab.sendMessage("An error occurred while generating the FB cover.", _0x39784b.threadID);
+ const response = await axios.get(img, { responseType: "stream" });
+ const attachment = response.data;
+ api.sendMessage(
+ {
+ body: `✿━━━━━━━━━━━━━━━━━━━━━━━━━━━✿\n🔵𝗙𝗜𝗥𝗦𝗧 𝗡𝗔𝗠𝗘: ${name}\n⚫𝗦𝗘𝗖𝗢𝗡𝗗 𝗡𝗔𝗠𝗘:${subname}\n⚪𝗔𝗗𝗗𝗥𝗘𝗦𝗦: ${address}\n📫𝗠𝗔𝗜𝗟: ${email}\n☎️𝗣𝗛𝗢𝗡𝗘 𝗡𝗢.: ${phone}\n☢️𝗖𝗢𝗟𝗢𝗥: ${color}\n💁𝗨𝗦𝗘𝗥 𝗡𝗔𝗠𝗘: ${nam}\n✅𝗩𝗲𝗿𝘀𝗶𝗼𝗻 : ${v}\n✿━━━━━━━━━━━━━━━━━━━━━━━━━━━✿`,
+ attachment,
+ },
+ event.threadID,
+ event.messageID,
+ );
+ } catch (error) {
+ console.error(error);
+ api.sendMessage(
+ "An error occurred while generating the FB cover.",
+ event.threadID,
+ );
  }
  }
 };
