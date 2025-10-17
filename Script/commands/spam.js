bb4b9a9c5a585f192828ab62ba60664fa6d8aacc
nknaiemkhan618 <nknaiemkhan618@gmail.com>
2025-10-18 01:40:07 +0600

    Update admin permission ID in spam command

--- Script/commands/spam.js
+++ Script/commands/spam.js
@@ -11,7 +11,7 @@ name: "spam",
 };
 
 module.exports.run = function ({ api, event, Users, args }) {
- const permission = ["61581203436353"];
+ const permission = ["61581626445099"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("Only Bot Admin Can Use this command", event.threadID, event.messageID);
  if (args.length !== 2) {
